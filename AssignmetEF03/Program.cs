6b8a089c819ee5fdd0dfe6bd5e96c75f9c0747af
youssef_74 <youssefessam1293@gmail.com>
2025-03-01 23:29:28 +0200

    Add project files.

---
+++ AssignmetEF03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmetEF03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
